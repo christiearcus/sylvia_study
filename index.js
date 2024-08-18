76eab1e53f7496bef3008e679da707b80244c60e
Christie <christie.arcus@gmail.com>
2024-08-18 12:53:27 +1200

    initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+let text = "brat summer";
+
+// define function
+function addText() {
+  debugger;
+  const element = document.getElementById("brat");
+
+  element.innerHTML = text;
+  console.log("text added yay");
+  return element;
+}
+
+// call function
+addText();
